f99944c75551e473872e1d1721f05f194776aef8
DESKTOP\David <Anderson.David@protonmail.com>
2018-06-08 22:54:27 -0500

    Implimented LOD & fixed mesh seams

--- World Explore/Assets/Editor/MapGeneratorEditor.cs
+++ World Explore/Assets/Editor/MapGeneratorEditor.cs
@@ -15,12 +15,12 @@ public class MapGeneratorEditor : Editor
         {
             if (mapGen.autoUpdate)
             {
-                mapGen.GenerateMap();
+                mapGen.DrawMapInEditor();
             }
         }
         if (GUILayout.Button("Generate"))
         {
-            mapGen.GenerateMap();
+            mapGen.DrawMapInEditor();
         }
     }
 }
